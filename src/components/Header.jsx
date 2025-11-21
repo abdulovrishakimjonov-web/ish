060f9a74eca28861048c2af89e8e267cd78961bc
Your Name <your.email@example.com>
2025-11-21 11:02:58 +0500

    qoshldi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import abujon from '../img/abujon.png'
 
 const Header = () => {
   return (
     <div className='flex justify-between p-3 px-4 bg-slate-700'>
-        <div>LOGO</div>
+        <div><img className='w-[50px]' src={abujon} alt="" /></div>
         <div>
             <ul className='flex gap-6'>
                 <NavLink className={({isActive}) => (isActive ? "text-neutral-100" : "")} to="/">Home</NavLink>
@@ -13,7 +14,7 @@ const Header = () => {
                 <NavLink className={({isActive}) => (isActive ? "text-neutral-100" : "")} to="/blog">Blog</NavLink>
             </ul>
         </div>
-        <div><button>Clic</button></div>
+        <div><button className='w-[200px] p-2 hover:bg-cyan-950 hover:text-amber-50 rounded-md bg-cyan-100 '>Clic</button></div>
     </div>
   )
 }
